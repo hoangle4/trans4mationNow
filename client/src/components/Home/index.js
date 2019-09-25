f2bfb1c9dc2b2074b4bdb7506a15fa6e0c82f208
hoangle4 <hoang.le4@pcc.edu>
2019-09-25 02:19:53 -0700

    commit

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,12 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Navbar from './Navbar';
 import Landing from './Landing';
 import './Home.css';
+import Utils from '../utils';
 const Home = () => {
+  useEffect(() => {
+    Utils.test();
+  }, []);
   return (
     <Fragment>
       <Navbar />
