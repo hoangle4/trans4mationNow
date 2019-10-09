768f4ca270799f5da3336d1cd895e0c46e494cf6
hoangle4 <hoang.le4@pcc.edu>
2019-10-08 21:20:03 -0700

    commit links

--- client/src/components/Home/Navbar.js
+++ client/src/components/Home/Navbar.js
@@ -1,59 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => (
-	<nav className='Navbar navbar navbar-expand-lg navbar-light font-weight-bold fixed-top'>
-		<a className='navbar-brand' href='/'>
-			<img src='images/logo-clear.png' alt='trans4mation logo' className='Navbar_logo' />
-		</a>
-		<h5 className='Navbar_h5'>G.E.M.s University</h5>
-		<button
-			className='navbar-toggler'
-			type='button'
-			data-toggle='collapse'
-			data-target='#navbarSupportedContent'
-			aria-controls='navbarSupportedContent'
-			aria-expanded='false'
-			aria-label='Toggle navigation'
-		>
-			<span className='navbar-toggler-icon' />
-		</button>
+  <nav className='Navbar navbar navbar-expand-lg navbar-light font-weight-bold fixed-top'>
+    <Link className='navbar-brand' to='/'>
+      <img
+        src='images/logo-clear.png'
+        alt='trans4mation logo'
+        className='Navbar_logo'
+      />
+    </Link>
+    <h5 className='Navbar_h5'>G.E.M.s University</h5>
+    <button
+      className='navbar-toggler'
+      type='button'
+      data-toggle='collapse'
+      data-target='#navbarSupportedContent'
+      aria-controls='navbarSupportedContent'
+      aria-expanded='false'
+      aria-label='Toggle navigation'
+    >
+      <span className='navbar-toggler-icon' />
+    </button>
 
-		<div className='collapse navbar-collapse justify-content-end' id='navbarSupportedContent'>
-			<ul className='navbar-nav'>
-				<li className='nav-item'>
-					<a className='nav-link' href='/aboutus'>
-						About Us
-					</a>
-				</li>
+    <div
+      className='collapse navbar-collapse justify-content-end'
+      id='navbarSupportedContent'
+    >
+      <ul className='navbar-nav'>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/aboutus'>
+            About Us
+          </Link>
+        </li>
 
-				<li className='nav-item'>
-					<a className='nav-link' href='#!'>
-						Resources
-					</a>
-				</li>
-				<li className='nav-item'>
-					<a className='nav-link' href='#!'>
-						Get Involved
-					</a>
-				</li>
-				<li className='nav-item'>
-					<Link className='nav-link' to='/event'>
-						Events
-					</Link>
-				</li>
-				<li className='nav-item'>
-					<a className='nav-link' href='#!'>
-						Donations
-					</a>
-				</li>
-				<li className='nav-item'>
-					<a className='nav-link' href='/contactus'>
-						Contact Us
-					</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/resources'>
+            Resources
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/getinvolved'>
+            Get Involved
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/event'>
+            Events
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className='nav-link' to='donation'>
+            Donations
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/contactus'>
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
