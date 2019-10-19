a388b9fab927efcf3636cfbeac7b5589ac963f54
hoangle4 <hoang.le4@pcc.edu>
2019-10-18 21:48:06 -0700

    commit layout

--- client/src/components/Home/LandingStory.js
+++ client/src/components/Home/LandingStory.js
@@ -14,11 +14,26 @@ const LandingStory = () => {
           </div>
           <div className='col-md-8 col-xs-12'>
             <p className='card-text'>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Excepturi quo, dolore, voluptate
-              numquam iusto tenetur nam, modi dignissimos atque eum illo rerum
-              qui ipsum laudantium!
+              <strong>
+                I was not going to see my grandbaby be a statistic’:
+                Grandparents stepping up amid the opioid crisis in Ohio
+              </strong>
+              <br />
+              CLEVELAND, Ohio – Thousands of people in Ohio have died from
+              opioid overdoses in the last ten years. According to the Centers
+              for Disease Control in 2017, 4,293 people died as a result of
+              overdoses involving prescription and synthetic opioids and heroin.
+              The massive number of deaths has left thousands of children
+              without their parents. Some are put into foster care, but in many
+              cases grandparents are stepping up to take responsibility. Mildra
+              Kelly, 65, lives in Cleveland and is raising her 13-year-old
+              grandson, Brian
+              <a
+                href='https://fox8.com/2019/10/11/i-was-not-going-to-see-my-grandbaby-be-a-statistic-grandparents-stepping-up-amid-the-opioid-crisis-in-ohio/'
+                target='_blank'
+              >
+                ...Read more.
+              </a>
             </p>
           </div>
         </div>
@@ -27,11 +42,30 @@ const LandingStory = () => {
         <div className='row'>
           <div className='col-md-8 col-xs-12'>
             <p className='card-text'>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Veritatis reiciendis perspiciatis accusamus
-              soluta maiores! At eos illum nemo doloribus rerum quod aliquid,
-              ratione officiis inventore? Minus, culpa? Voluptatem, sint dolor!
+              <strong>
+                Grandparents Advisory Council Created by Brown's Supporting
+                Grandparents Raising Grandchildren Act Holds First Meeting
+              </strong>
+              <br />
+              <strong>Wednesday, September 11, 2019</strong>
+              <br />
+              WASHINGTON, D.C. – U.S. Senator Sherrod Brown (D-OH) is applauding
+              news that the Department of Health and Human Services (HHS)
+              Administration for Community Living (ACL), held the first
+              Grandparents Advisory Council meeting last week. Brown’s
+              Supporting Grandparents Raising Grandchildren Act was signed into
+              law last year and created the council in light of the addiction
+              crises, which has forced many grandparents and other family
+              members to step in and raise children whose parents are unable to
+              care for them. The council met last week to discuss the needs of
+              grandparents raising children and develop strategies on how
+              federal agencies can best serve them.
+              <a
+                href='https://www.brown.senate.gov/newsroom/press/release/grandparents-advisory-council-created-by-browns-supporting-grandparents-raising-grandchildren-act-holds-first-meeting'
+                target='_blank'
+              >
+                ...Read more.
+              </a>
             </p>
           </div>
           <div className='col-md-3 col-xs-12'>
@@ -54,12 +88,27 @@ const LandingStory = () => {
           </div>
           <div className='col-md-8 col-xs-12'>
             <p className='card-text'>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Veritatis reprehenderit obcaecati
-              voluptas hic impedit sequi ut ipsum nam! Earum harum
-              necessitatibus aut perspiciatis ducimus adipisci unde assumenda
-              esse, consequatur, deleniti nam, rem quo!
+              <strong>Maya Angelou talks about grandparenting</strong>
+              <br />
+              <strong>” I loved my grandmother.”</strong>
+              <br />
+              The memories flow out of Maya Angelou’s words like sweet molasses.
+              It’s a tale she’s told before — in her groundbreaking work, I Know
+              Why the Caged Bird Sings (Random House). But now, at 80, Angelou,
+              originally from Stamps, Ark., is a grandmother twice over. Her
+              son, Guy Johnson, is father to Colin Ashanti Murphy-Johnson, 32,
+              and Elliott Jones, 22. She’s also a great grandmother — Colin has
+              two children, Caylin Nicole Johnson, 11, and Brandon Bailey
+              Johnson, 9. Angelou talked to Grandparents.com about her own
+              grandmother’s legacy and how it shaped her thoughts about
+              grandparenting.
+              <a
+                href='https://considerable.com/maya-angelou-words/'
+                target='_blank'
+              >
+                ...Read more.
+              </a>
+              <br />
             </p>
           </div>
         </div>
