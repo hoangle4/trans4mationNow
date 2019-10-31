8054e55694cc95d4855fc5811b5801684aea834d
hoangle4 <hoang.le4@pcc.edu>
2019-10-31 09:12:58 -0700

    commit placeholder

--- client/src/components/Survey/SurveyInputItem.js
+++ client/src/components/Survey/SurveyInputItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SurveyInputItem = ({ label, value, choiceName, setValue }) => {
+const SurveyInputItem = ({
+  label,
+  value,
+  choiceName,
+  setValue,
+  placeholder
+}) => {
   return (
     <div className='mb-5'>
       <h4 className=''>{label}</h4>
@@ -9,7 +15,7 @@ const SurveyInputItem = ({ label, value, choiceName, setValue }) => {
         name={choiceName}
         className='form-control'
         id={choiceName}
-        placeholder='Enter text here'
+        placeholder={placeholder}
         value={value}
         onChange={setValue}
       ></textarea>
@@ -21,7 +27,8 @@ SurveyInputItem.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   choiceName: PropTypes.string.isRequired,
-  setValue: PropTypes.func.isRequired
+  setValue: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired
 };
 
 export default SurveyInputItem;
