5ad178dbc18957721334af4497b7e5b23d28b265
Hoang Le <hoang.le4@pcc.edu>
2019-12-01 13:26:01 -0800

    commit Dockerfile

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require("./routes");
+const routes = require('./routes');
 const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://master.dnsy5i4as8f52.amplifyapp.com'
-];
+const allowedOrigins = [ 'http://localhost:3000', 'https://master.dnsy5i4as8f52.amplifyapp.com' ];
 
 app.use(
-  cors({
-    // origin: function(origin, callback) {
-    //   // allow requests with no origin
-    //   // (like mobile apps or curl requests)
-    //   if (!origin) return callback(null, true);
-    //   if (allowedOrigins.indexOf(origin) === -1) {
-    //     var msg =
-    //       "The CORS policy for this site does not " +
-    //       "allow access from the specified Origin.";
-    //     return callback(new Error(msg), false);
-    //   }
-    //   return callback(null, true);
-    // }
-    origin: '*'
-  })
+	cors({
+		// origin: function(origin, callback) {
+		//   // allow requests with no origin
+		//   // (like mobile apps or curl requests)
+		//   if (!origin) return callback(null, true);
+		//   if (allowedOrigins.indexOf(origin) === -1) {
+		//     var msg =
+		//       "The CORS policy for this site does not " +
+		//       "allow access from the specified Origin.";
+		//     return callback(new Error(msg), false);
+		//   }
+		//   return callback(null, true);
+		// }
+		origin: '*'
+	})
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+	require('dotenv').config();
 }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+	app.use(express.static('client/build'));
 }
 
 //Backend API
@@ -54,16 +51,15 @@ if (process.env.NODE_ENV === 'production') {
 ///////
 app.use(routes);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transformation",{ useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transformation",{ useNewUrlParser: true });
 
 /////
 
-
 app.listen(PORT, function() {
-  console.log(`API Server now listening on PORT ${PORT}!`);
+	console.log(`API Server now listening on PORT ${PORT}!`);
 });
